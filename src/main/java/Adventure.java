3ee4961e05c425a10e4829d57561a4cd1a1802ad
SimonBD1 <simo20k3@stud.kea.dk>
2022-09-27 09:01:32 +0200

    Commit and åpush

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -1,3 +1,5 @@
 public class Adventure {
-
+Room selectedRoom = null;
+selectedRoom=selectedRoom.getEast
+    }
 }
--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -3,4 +3,20 @@ public class Room {
     private String north;
     private String east;
     private String west;
+
+    public void setEast(String east) {
+        this.east = east;
+    }
+
+    public void setNorth(String north) {
+        this.north = north;
+    }
+
+    public void setSouth(String south) {
+        this.south = south;
+    }
+
+    public void setWest(String west) {
+        this.west = west;
+    }
 }
