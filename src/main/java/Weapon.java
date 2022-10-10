ea14a627e079c2d0b713a7274c59d316b11c6a21
Tobias <tobi4533@stud.kea.dk>
2022-10-10 12:44:08 +0200

    Added mandag d 10 1.0

---
+++ src/main/java/Weapon.java
@@ -0,0 +1,18 @@
+public class Weapon extends Item {
+
+    private int healthPoints;
+
+
+    public Weapon
+            (String name, String description, int healthPoints) {
+        super(name, description);
+        this.healthPoints = healthPoints;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+    public String getDescription () {
+        return getItemDescription();
+    }
+}
