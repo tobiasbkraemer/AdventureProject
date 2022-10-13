586f01321c7f39d542ade4e6d62010afb2f7d980
kjartanLeander <Kjar0321@stud.kea.dk>
2022-10-13 10:06:21 +0200

    update story

--- src/main/java/Enemy.java
+++ src/main/java/Enemy.java
@@ -25,10 +25,6 @@ public class Enemy {
 
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public String getName() {
         return name;
     }
@@ -43,10 +39,6 @@ public class Enemy {
 
     public Weapon getWeapon() {
         return weapon;
-    }
-
-    public void hit(int newHealth) {
-        this.healthPoints = newHealth;
     }
 
     public boolean isDead(){
